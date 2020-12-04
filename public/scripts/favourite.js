67a37b719bfef29100ac1c74812842fea8d7ae8a
vagrant <avedj94@hotmail.com>
2020-12-04 05:34:42 +0000

    add fav message when fav button is clicked

--- public/scripts/favourite.js
+++ public/scripts/favourite.js
@@ -12,5 +12,13 @@ $(() => {
     .catch(err => {
       console.log(err);
     })
+    $(".favourites").css({
+      "display": "flex",
+      "justify-content": "center",
+      "font-size": '20px',
+      "background-color": 'rgb(169,169,169)',
+      "color": "white",
+      "padding": '15px'
+    }).delay(1000).slideUp(3000);
   })
 })
